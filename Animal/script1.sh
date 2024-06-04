edadc8de1ae2b18de44b627577ff3217e16ef11c
GIGANEKIT NIKITA <Kotrunov.nikita@gmail.com>
2024-06-04 07:45:30 +0000

    3 #3

---
+++ Animal/script1.sh
@@ -0,0 +1,16 @@
+bash#!/bin/bash
+
+
+
+cd /193group/Animal/Heit
+
+
+animals=("Кошка" "Собака" "Лев" "Змея" "Кролик" "Слон" "Попугай" "Медведь" "Обезьяна" "Тигр" "Крокодил" "Панда" "Жираф" "Волк" "Лиса" "Олень" "Хомяк" "Корова" "Коза" "Лошадь")
+
+for ((i=1; i<=20; i++)); do
+    touch file_$i.txt
+    for ((j=0; j<20; j++)); do
+        animal_index=$((RANDOM % ${#animals[@]}))
+        echo ${animals[$animal_index]} >> file_$i.txt
+    done
+done
